c03347635e3619c2953e36b80a21bc073cff5207
lsyokoo <lsy20041203@gmail.com>
2024-10-28 18:20:34 -0400

    change

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ const port = process.env.PORT || 3001
 
 // Set up a response for the root path of the application
 app.get('/', (req, res) => {
-  res.send("Write an instruction for a drawing here. For example: draw a line across the page, draw 10 circles of different sizes, draw a duck.")
+  res.send("Hi I'm Siyu.")
 })
 
+app.get('/country', (req, res) => {
+  const countryName = req.query.country; // Access the country name
+  res.send(`Country: ${countryName}`);
+});
+
+app.get('/movies', (req, res) => {
+  const genreName = req.query.genre; // Access the genre name
+  res.send(`genre: ${genreName}`);
+});
 // Set the application to listen a port
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}` )
