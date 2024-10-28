fa7c2b33cc435e40b8e59d7d0379988b10ec1405
lsyokoo <lsy20041203@gmail.com>
2024-10-28 17:02:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hi!")
+// Import the express library and assign it to a variable
+import express from 'express'
+
+// Create an instance of an express application 
+const app = express()
+
+// Set the port the application will be running on
+const port = process.env.PORT || 3001
+
+// Set up a response for the root path of the application
+app.get('/', (req, res) => {
+  res.send("Write an instruction for a drawing here. For example: draw a line across the page, draw 10 circles of different sizes, draw a duck.")
+})
+
+// Set the application to listen a port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}` )
+})
